49134489fadb64013df8b347ab3a7d10df71b738
Rob Korich <tennisrob@gmail.com>
2020-06-16 20:52:08 -0700

    js pokemonList code format fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 var pokemonList = [
-    {name: 'Charmander', height: 2, type: 'fire'},
-    {name: 'Pikachu', height: 1, type: 'electric'},
-    {name: 'Squirtle', height: 1.5, type: 'water'}
+    {
+        name: 'Charmander',
+        height: 2, 
+        type: ['fire']
+    },
+    {
+        name: 'Pikachu', 
+        height: 1, 
+        type: ['electric']
+    },
+    {
+        name: 'Squirtle', 
+        height: 1.5, 
+        type: ['water']
+    }
 ];
