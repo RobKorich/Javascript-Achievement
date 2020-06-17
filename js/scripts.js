b6874343d67032a26e455d305167dd3877e70ef2
Rob Korich <tennisrob@gmail.com>
2020-06-16 20:37:40 -0700

    js pokemonList update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-var favoriteFood = 'Pineapple';
-document.write(favoriteFood);
+var pokemonList = [
+    {name: 'Charmander', height: 2, type: 'fire'},
+    {name: 'Pikachu', height: 1, type: 'electric'},
+    {name: 'Squirtle', height: 1.5, type: 'water'}
+];
