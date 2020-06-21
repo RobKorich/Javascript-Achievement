d7546cb3e29233e3f422e64e92a4a6824132ff0b
Rob Korich <tennisrob@gmail.com>
2020-06-21 16:30:05 -0700

    forEach to for loop change

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,16 @@ var pokemonList = [
     }
 ];
 
+pokemonList.forEach(function(pokemon){
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')')
+    if (pokemon.height > 1) {
+        document.write(' - Wow, thats\'s big!')
+    }
+    document.write('</p>')
+})
+
+/* reference code for later (old version)
+
 for (var i = 0; i < pokemonList.length; i++) {
     var className = 'otherPokemon'
     if (pokemonList[i].name === 'Charmander') {
@@ -27,3 +37,4 @@ for (var i = 0; i < pokemonList.length; i++) {
     }
     document.write('</p>')
 }
+*/
