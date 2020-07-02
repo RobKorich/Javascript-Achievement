dd85ea346067ea189cd067dd63d9c17f805b7ff8
Rob Korich <tennisrob@gmail.com>
2020-07-02 10:39:03 -0700

    JS function changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var pokemonRepository = (function () {
+var pokemonRepository = (function () { /*IIFE start*/
     var pokemonList = [
         {
             name: 'Charmander',
@@ -25,19 +25,38 @@ var pokemonRepository = (function () {
         return pokemonList;
     }
 
+    function showDetails(pokemon) { /*why does pokemon go here?*/
+        console.log(pokemon); /*??*/
+    }
+
+    function addListItem(pokemon) {
+        var pokedexList = document.querySelector('.pokemon-list'); /*selects <ul> in HTML and assigns it as the pokedexList variable*/
+        var listItem = document.createElement('li'); /*creates the <li> element and assigns it as the listItem variable*/
+        var button = document.createElement('button'); /* creates a <button> element and assings it as the button variable*/
+        button.innerText = pokemon.name; /*?? got it to work but why is it pokemon and not pokemonList again*/
+        button.classList.add('pokedexButton'); /*adds a class name 'pokedexButton' to button so it can be edited in css*/
+        button.addEventListener('click', showDetails(pokemon));
+        listItem.appendChild(button); /*states parent element then adds child button*/
+        pokedexList.appendChild(listItem);/*states parent element then adds child listItem*/
+    }
+
+    /*only things visible outside of IIFE*/
     return {
         add,
-        getAll
+        getAll,
+        addListItem
     }
-})();
+})(); /*IIFE end*/
 
 pokemonRepository.getAll().forEach(function(pokemon){
-    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')')
-    if (pokemon.height > 1) {
-        document.write(' - Wow, thats\'s big!')
-    }
-    document.write('</p>')
+    pokemonRepository.addListItem(pokemon);
 })
+
+/*document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')')
+if (pokemon.height > 1) {
+    document.write(' - Wow, thats\'s big!')
+}
+document.write('</p>')*/
 
 /* reference code for later (old version)
 
@@ -55,12 +74,13 @@ for (var i = 0; i < pokemonList.length; i++) {
 */
 
 /* how to add a pokemon
-
 console.log(pokemonRepository.getAll());
+
 pokemonRepository.add({
     name: 'Bulbasaur',
     height: 1,
     type: ['poison']
 });
+
 console.log(pokemonRepository.getAll());
 */
